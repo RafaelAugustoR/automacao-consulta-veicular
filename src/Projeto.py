b4905bcfc0a186fad2cad1d6ee894603365c2318
Rafael <rafaelaugustodev@gmail.com>
2023-12-17 01:03:00 -0300

    explicações para melhor entendimento

--- src/Projeto.py
+++ src/Projeto.py
@@ -10,6 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def esperar_elemento(driver, by, valor, timeout=10):
     try:
+        # Aguarda até que o elemento esteja presente na página
         elemento = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((by, valor))
         )
@@ -21,20 +22,27 @@ def esperar_elemento(driver, by, valor, timeout=10):
 def extrair_dados(driver, xpaths):
     dados = []
     for xpath in xpaths:
+        # Extrai dados dos elementos correspondentes aos XPaths fornecidos
         elemento = esperar_elemento(driver, By.XPATH, xpath)
         dados.append(elemento.text if elemento else None)
     return dados
 
+# Configurando o WebDriver
 servico = Service(ChromeDriverManager().install())
 arquivoExcel = 'Demo.xlsx'
 urlDetran = "https://consultas.detrannet.sc.gov.br/servicos/consultaveiculo.asp"
 
+# Atribui ao data frame o arquivo excel
 df = pd.read_excel(arquivoExcel)
+
+# Inicialização do WebDriver, Workbook e Worksheet
 chrome = webdriver.Chrome(service=servico)
 wb = load_workbook(arquivoExcel)
 ws = wb.active
 
+# Percorre as linhas do DataFrame
 for index, row in df.iterrows():
+    # Aqui é verificado se a linha contém informações
     if pd.isna(row["PLACA"]):
         print("Não há mais informações. Interrompendo o loop.")
         break
@@ -44,16 +52,15 @@ for index, row in df.iterrows():
     elementoPlaca = esperar_elemento(chrome, By.XPATH, '//*[@id="placa"]')
     elementoRenavam = esperar_elemento(chrome, By.XPATH, '//*[@id="renavam"]')
 
-    if not elementoPlaca or not elementoRenavam:
-        print("Elementos não encontrados. Verifique os XPaths.")
-        break
-
+    # Preenche os campos com os valores do DataFrame
     elementoPlaca.send_keys(row["PLACA"])
     elementoRenavam.send_keys(row["RENAVAM"])
 
+    # Clica no botão de consulta
     chrome.find_element(By.XPATH, '//*[@id="form1"]/table[2]/tbody/tr[4]/td/fieldset/table/tbody/tr[4]/td/button').click()
 
     try:
+        # Aguarda até que o elemento da tabela esteja presente na página
         elementoTabela = esperar_elemento(chrome, By.XPATH, '//*[@id="div_servicos_09"]')
         if not elementoTabela:
             print("ElementoTabela não encontrado. Verifique o XPath ou aguarde a página carregar completamente.")
@@ -69,14 +76,17 @@ for index, row in df.iterrows():
         '//*[@id="div_servicos_09"]/table/tbody/tr[5]/td[4]/table/tbody/tr[1]/td'
     ]
 
+    # Encontra a próxima coluna vazia
     col_num = 1
     while ws.cell(row=index + 2, column=col_num).value is not None:
         col_num += 1
 
+    # Extrai os dados e os adiciona à próxima coluna vazia na linha atual
     dados = extrair_dados(chrome, xpaths)
     for i, data in enumerate(dados):
         ws.cell(row=index + 2, column=col_num + i, value=data)
 
+    # Salva os dados
     wb.save(arquivoExcel)
 
 chrome.quit()
